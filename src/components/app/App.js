9207b478d6feaa4ce661fe7966c11bf8789c8214
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-11-04 17:05:50 -0500

    deleted dead code and id prop for Card

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,66 +10,39 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [notification, setNotification] = useState();
 
-  // const [test, setTest] = useState("test goes here");
-
-  // useEffect(() => {
-  //     fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest')
-  // // getUsersData();
-  // });
-
-  // const addUser = (user) => {
-  //   setUsers(prevUsers => [...prevUsers, user])
-  // }
-
   const getUsersData = async () => {
     setNotification("");
     setLoading(true);
     const response = await fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest');
+
     if (response.ok) {
       const fetchedUsers = await response.json();
       const userArray = [];
+
       for (let i = 0; i < fetchedUsers.length; i++) {
-
-        // let userResponse = 
         let user = await fetchedUsers[i]['user' + (i+1)];
-        // let id = user.id;
-        // let name = user.name;
-        // let image = user.image;
-        
-        // console.log(fetchedUsers[i]);
-        // console.log(id);
-        // console.log(name);
-        // console.log(image);
-        // console.log("***");
-
         userArray.push(user);
       }
       setUsers(userArray);
       setLoading(false);
-    } else {
+
+    } else if (response.status >= 400){
       console.log("oopsie poopsie you did a whoopsie");
       setUsers([]);
       setLoading(false);
       setNotification('Oops something went wrong');
-      // throw new Error('Something went wrong');
     }
-    
   }
-
-  // const testUsers = () => {
-  //   setTest("wait for it...")
-  //   setTimeout(() => [setTest(users.length), console.log(users)], 1000);
-  // }
 
   return (
     <div>
-      {/* <div>{test}</div> */}
       <div className={styles.center}>
         <button className={styles.button} onClick= {getUsersData}>populate cards</button>
-        {/* <button onClick= {testUsers}>testUsersState</button> */}
       </div>
       <h3 className={styles.center}>{notification}</h3>
-        {loading ? <img src={loadImg} alt="loading" className={styles.loadImg} /> : users.map((user, index) => <Card id={user.id} name={user.name} image={user.image} key={index} />)}
+        {loading ? <img src={loadImg} alt="loading" className={styles.loadImg} /> : users.map(
+          (user, index) => <Card name={user.name} image={user.image} key={index} />
+        )}
     </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,14 +2,12 @@ import React from 'react';
 import styles from './Card.module.css';
 
 const Card = (props) => {
-
-    const {id, name, image} = props;
+    const {name, image} = props;
 
     return (
         <div className={styles.card}>
             <img className={styles.image} src={image} alt="card image" />
             <div>{name}</div>
-            {/* <div>{id}</div> */}
         </div>
     );
 }
