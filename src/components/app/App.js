d6e22e20e42bb774b0706462d7ee29ad084751b3
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-11-03 20:09:13 -0500

    added some more testing code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,28 +4,53 @@ import styles from './App.module.css';
 const App = () => {
 
   const [users, setUsers] = useState([]);
+  const [test, setTest] = useState("test goes here");
 
   // useEffect(() => {
   //     fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest')
+  // // getUsersData();
   // });
 
+  // const addUser = (user) => {
+  //   setUsers(prevUsers => [...prevUsers, user])
+  // }
+
   const getUsersData = async () => {
+    console.log(users);
       const response = await fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest');
-      const users = await response.json();
-      for (let i = 0; i < users.length; i++) {
+      const fetchedUsers = await response.json();
+      const userArray = [];
+      for (let i = 0; i < fetchedUsers.length; i++) {
+
         // let userResponse = 
-        let user = users[i]['user' + (i+1)];
-        console.log(users[i]);
-        console.log(user.id);
-        console.log(user.name);
-        console.log(user.image);
-        console.log("***");
+        let user = fetchedUsers[i]['user' + (i+1)];
+        // let id = user.id;
+        // let name = user.name;
+        // let image = user.image;
+        
+        // console.log(fetchedUsers[i]);
+        // console.log(id);
+        // console.log(name);
+        // console.log(image);
+        // console.log("***");
+
+        userArray.push(fetchedUsers[i]);
       }
+      console.log(userArray);
+      setUsers(userArray);
+      console.log(users);
+      console.log("***");
+  }
+
+  const testUsers = () => {
+    setTest(users.length);
   }
 
   return (
     <div>
-      <button onClick= {getUsersData}>test</button>
+    <div>{test}</div>
+      <button onClick= {getUsersData}>JSONConsoleTest</button>
+      <button onClick= {testUsers}>testUsersState</button>
     </div>
   );
 }
