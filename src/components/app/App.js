7d3695805e0e9fddf3b4254daa68742006ee9960
Nicolas Hemenway <nhemenway@catalystsolves.com>
2020-11-04 12:55:48 -0500

    tweaked css, added loading spinner

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
+import loadImg from './ajax-loader.gif';
 
 import Card from '../card/Card';
 
 const App = () => {
 
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [test, setTest] = useState("test goes here");
 
   // useEffect(() => {
@@ -18,26 +20,28 @@ const App = () => {
   // }
 
   const getUsersData = async () => {
-      const response = await fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest');
-      const fetchedUsers = await response.json();
-      const userArray = [];
-      for (let i = 0; i < fetchedUsers.length; i++) {
+    setLoading(true);
+    const response = await fetch('https://5fa06868e21bab0016dfd1c6.mockapi.io/UsersRequest');
+    const fetchedUsers = await response.json();
+    const userArray = [];
+    for (let i = 0; i < fetchedUsers.length; i++) {
 
-        // let userResponse = 
-        let user = await fetchedUsers[i]['user' + (i+1)];
-        // let id = user.id;
-        // let name = user.name;
-        // let image = user.image;
-        
-        // console.log(fetchedUsers[i]);
-        // console.log(id);
-        // console.log(name);
-        // console.log(image);
-        // console.log("***");
+      // let userResponse = 
+      let user = await fetchedUsers[i]['user' + (i+1)];
+      // let id = user.id;
+      // let name = user.name;
+      // let image = user.image;
+      
+      // console.log(fetchedUsers[i]);
+      // console.log(id);
+      // console.log(name);
+      // console.log(image);
+      // console.log("***");
 
-        userArray.push(user);
-      }
-      setUsers(userArray);
+      userArray.push(user);
+    }
+    setUsers(userArray);
+    setLoading(false);
   }
 
   const testUsers = () => {
@@ -53,15 +57,8 @@ const App = () => {
         <button onClick= {getUsersData}>ConsoleTest + setState</button>
         <button onClick= {testUsers}>testUsersState</button>
       </div>
-        {/* <Card id={1} name="nameTest1" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/>
-        <Card id={2} name="nameTest2" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/>
-        <Card id={3} name="nameTest3" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/>
-        <Card id={4} name="nameTest4" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/>
-        <Card id={5} name="nameTest5" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/>
-        <Card id={6} name="nameTest6" image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/1200px-Test-Logo.svg.png"/> */}
-        <div>
-          {users.map((user, index) => <Card id={user.id} name={user.name} image={user.image} key={index} />)}
-        </div>
+
+        {loading ? <img src={loadImg} alt="loading" class={styles.loadImg} /> : users.map((user, index) => <Card id={user.id} name={user.name} image={user.image} key={index} />)}
     </div>
   );
 }
